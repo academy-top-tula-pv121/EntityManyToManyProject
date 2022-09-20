a072d3317297d2aa380f1ce881ce54f149951f54
admin <admin@VTORAYA0>
2022-09-20 21:23:59 +0300

    Добавьте файлы проекта.

---
+++ AmbulanceContext.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityManyToManyProject
+{
+    class Doctor
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public List<Patient> Patients { set; get; } = new();
+        public List<Service> Services { set; get; } = new();
+    }
+
+    class Patient
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public List<Doctor> Doctors { set; get; } = new();
+        public List<Service> Services { set; get; } = new();
+    }
+
+    class Service
+    {
+        public int DoctorId { get; set; }
+        public Doctor? Doctors { get; set; }
+
+        public int PatientId { get; set; }
+        public Patient? Patients { get; set; }
+
+        public int Room { get; set; }
+    }
+    class AmbulanceContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; } = null!;
+        public DbSet<Patient> Patients { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Initial Catalog=UsersDb;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Doctor>()
+                        .HasMany(d => d.Patients)
+                        .WithMany(p => p.Doctors)
+                        
+                        //.UsingEntity<Service>
+                        //(
+                        //    s => s.HasOne(dp => dp.Doctors)
+                        //            .WithMany(d => d.Services)
+                        //            .HasForeignKey(dp => dp.DoctorId),
+                        //    s => s.HasOne(dp => dp.Patients)
+                        //            .WithMany(p => p.Services)
+                        //            .HasForeignKey(dp => dp.PatientId),
+                        //    s =>
+                        //    {
+                        //        s.Property(dp => dp.Room).HasDefaultValue(5);
+                        //        s.HasKey(k => new { k.DoctorId, k.PatientId });
+                        //        s.ToTable("Registration");
+                        //    }
+                        //);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using EntityManyToManyProject;
+using Microsoft.EntityFrameworkCore;
+
+using (AmbulanceContext ambulance = new AmbulanceContext())
+{
+    ambulance.Database.EnsureDeleted();
+    ambulance.Database.EnsureCreated();
+
+    List<Doctor> doctors = new();
+    doctors.Add(new() { Name = "Aybolit" });
+    doctors.Add(new() { Name = "Livsi" });
+    doctors.Add(new() { Name = "Watson" });
+
+    List<Patient> patients = new();
+    patients.Add(new() { Name = "Tom" });
+    patients.Add(new() { Name = "Bob" });
+    patients.Add(new() { Name = "Joe" });
+    patients.Add(new() { Name = "Sam" });
+    patients.Add(new() { Name = "Jim" });
+    patients.Add(new() { Name = "Tim" });
+
+    ambulance.Doctors.AddRange(doctors);
+    ambulance.Patients.AddRange(patients);
+
+    doctors[0].Patients.Add(patients[4]);
+    doctors[0].Patients.Add(patients[1]);
+
+    doctors[1].Patients.Add(patients[0]);
+    doctors[1].Patients.Add(patients[2]);
+    doctors[0].Patients.Add(patients[5]);
+
+    doctors[2].Patients.Add(patients[1]);
+    doctors[2].Patients.Add(patients[3]);
+    doctors[2].Patients.Add(patients[5]);
+    doctors[2].Patients.Add(patients[4]);
+    doctors[2].Patients.Add(patients[2]);
+
+    ambulance.SaveChanges();
+
+}
+
+using (AmbulanceContext ambulance = new AmbulanceContext())
+{
+    var doctors = ambulance.Doctors
+                            .Include(d => d.Patients)
+                            .ToList();
+    foreach(var doctor in doctors)
+    {
+        Console.WriteLine(doctor.Name);
+        foreach(var patient in doctor.Patients)
+            Console.WriteLine($"\t{patient.Name}");
+    }
+
+    Console.WriteLine("-------------------------");
+
+    var patients = ambulance.Patients
+                            .Include(p => p.Doctors)
+                            .ToList();
+    foreach (var patient in patients)
+    {
+        Console.WriteLine(patient.Name);
+        foreach (var doctor in patient.Doctors)
+            Console.WriteLine($"\t{doctor.Name}");
+    }
+}
+
+Console.WriteLine("-------------------------");
+Console.WriteLine("-------------------------");
+
+using (AmbulanceContext ambulance = new AmbulanceContext())
+{
+    Doctor? doctor = ambulance.Doctors.FirstOrDefault(d => d.Name == "Livsi");
+    ambulance.Doctors.Remove(doctor);
+
+    Patient? patient = new() { Name = "Leo" };
+    var doctors = ambulance.Doctors.ToList();
+    patient.Doctors.Add(doctors[0]);
+    patient.Doctors.Add(doctors[2]);
+    ambulance.Patients.Add(patient);
+
+    ambulance.SaveChanges();
+}
+
+
+using (AmbulanceContext ambulance = new AmbulanceContext())
+{
+    var doctors = ambulance.Doctors
+                            .Include(d => d.Patients)
+                            .ToList();
+    foreach (var doctor in doctors)
+    {
+        Console.WriteLine(doctor.Name);
+        foreach (var patient in doctor.Patients)
+            Console.WriteLine($"\t{patient.Name}");
+    }
+
+    Console.WriteLine("-------------------------");
+
+    var patients = ambulance.Patients
+                            .Include(p => p.Doctors)
+                            .ToList();
+    foreach (var patient in patients)
+    {
+        Console.WriteLine(patient.Name);
+        foreach (var doctor in patient.Doctors)
+            Console.WriteLine($"\t{doctor.Name}");
+    }
+
+    //Console.WriteLine("-------------------------");
+
+    //patients = ambulance.Patients
+    //                    .Include(p => p.Doctors)
+    //                        .ThenInclude(d => d != null)
+    //                    .ToList();
+    //foreach (var patient in patients)
+    //{
+    //    Console.WriteLine(patient.Name);
+    //    foreach (var doctor in patient.Doctors)
+    //        Console.WriteLine($"\t{doctor.Name}");
+    //}
+}
